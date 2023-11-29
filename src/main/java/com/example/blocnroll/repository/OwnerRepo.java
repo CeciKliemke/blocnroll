ed86e2320bddd03f1347b0dc8374603a47211058
Cecília Chinaglia Kliemke <117697178+CeciKliemke@users.noreply.github.com>
2023-11-28 21:18:51 -0300

    Delete src directory

--- src/main/java/com/example/blocnroll/repository/OwnerRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.blocnroll.repository;
-
-import com.example.blocnroll.entities.Owner;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OwnerRepo extends JpaRepository<Owner, Long> {
-    
-}
