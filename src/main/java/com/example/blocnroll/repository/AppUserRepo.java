a874b6e22980fb5d24602a41459ac3656a22d255
Cecília Chinaglia Kliemke <117697178+CeciKliemke@users.noreply.github.com>
2023-12-03 22:54:05 -0300

    Delete src directory

--- src/main/java/com/example/blocnroll/repository/AppUserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.blocnroll.repository;
-
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.blocnroll.security.AppUser;
-
-public interface AppUserRepo extends JpaRepository<AppUser, Long>{
-    Optional<AppUser> findByEmail(String email);
-}
